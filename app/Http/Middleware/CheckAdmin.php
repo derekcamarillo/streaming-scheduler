69385ece697ed69b43a1add26437b81441e433df
topskilldev <me@topskill.dev>
2018-06-02 15:38:36 +0800

    Customer

---
+++ app/Http/Middleware/CheckAdmin.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\Auth;
+
+class CheckAdmin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if (!Auth::check())
+            return redirect('login');
+        if (Auth::user()->role != 1)
+            return redirect('home');
+
+        return $next($request);
+    }
+}
